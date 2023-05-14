2520ab3b096c5a4d0c4166e1b208765d7d9ac595
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-14 20:30:46 +0530

    added

---
+++ lib/screens/add_places.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => throw UnimplementedError();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
