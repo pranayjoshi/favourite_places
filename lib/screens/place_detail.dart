02512cd440b09cc6266f6ac2e126935216943e57
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-16 22:29:01 +0530

    added

---
+++ lib/screens/place_detail.dart
@@ -0,0 +1,18 @@
+import 'package:favourite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [], title: Text(place.title),),
+      body: Center(child: Text(place.title),),
+    );
+  }
+}
