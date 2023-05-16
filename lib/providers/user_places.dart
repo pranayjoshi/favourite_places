0f8916f75199bc5f8d0f3e974a9f531e0520717b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-16 17:14:41 +0530

    added

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -3,4 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>>{
   UserPlacesNotifier():super(const []);
+
+  void addPlace(String title){
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
 }
+
+final UserPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
