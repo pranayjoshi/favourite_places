8479e95af8977760825de657bfc057cb329ff563
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-16 22:36:28 +0530

    added

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      width: double.infinity,
+      child: TextButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.camera),
+          label: Text("Take Picture")),
+    );
+  }
+}
