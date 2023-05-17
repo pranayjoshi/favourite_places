6a5dec33f6c0a2bd0ee689f438ad99e0a646b88e
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-17 22:37:41 +0530

    added

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -14,37 +14,47 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   File? _selectedImage;
 
-  void _takePicture() async{
+  void _takePicture() async {
     final imagePicker = ImagePicker();
-    final pickImage = await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    final pickImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
 
-    if (pickImage==null){
+    if (pickImage == null) {
       return;
     }
 
     setState(() {
       _selectedImage = File(pickImage.path);
     });
-    
-
   }
+
   @override
   Widget build(BuildContext context) {
     Widget content = TextButton.icon(
-          onPressed: () {_takePicture();},
-          icon: Icon(Icons.camera),
-          label: Text("Take Picture"));
-    
-    if (_selectedImage != null){
-      content = Image.file(_selectedImage!, fit: BoxFit.cover, width: double.infinity, height: double.infinity,);
+        onPressed: () {
+          _takePicture();
+        },
+        icon: Icon(Icons.camera),
+        label: Text("Take Picture"));
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+          child: Image.file(
+        _selectedImage!,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      ));
     }
 
     return Container(
-      decoration: BoxDecoration(border: Border.all(width: 1, color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: content
-    );
+        decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
+        height: 250,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: content);
   }
 }
