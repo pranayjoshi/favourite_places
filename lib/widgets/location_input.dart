6c0954184c59dbefab8b4263ce4951815ca7fd0a
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-17 23:00:41 +0530

    added

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
