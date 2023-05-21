d746a8770cbde92964e829f0bd0d345a2b69f788
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-21 14:21:13 +0530

    added

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,8 +1,7 @@
-import 'package:favourite_places/models/place.dart';
-import 'package:favourite_places/screens/place_detail.dart';
+import '../screens/place_detail.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../models/place.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -13,29 +12,42 @@ class PlacesList extends StatelessWidget {
   Widget build(BuildContext context) {
     if (places.isEmpty) {
       return Center(
-          child: Text(
-        "No places yet!",
-        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-      ));
+        child: Text(
+          'No places added yet',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
     }
 
     return ListView.builder(
+      itemCount: places.length,
       itemBuilder: (ctx, index) => ListTile(
+        leading: CircleAvatar(
+          radius: 26,
+          backgroundImage: FileImage(places[index].image),
+        ),
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
-        leading: CircleAvatar(radius: 26, backgroundImage: FileImage(places[index].image),),
+        subtitle: Text(
+          places[index].location.address,
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
         onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (ctx) => PlaceDetailScreen(place: places[index])));
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
         },
       ),
-      itemCount: places.length,
     );
   }
 }
